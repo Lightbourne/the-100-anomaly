1235c8968778d77e4168c587ccd8102f1fbf6ab4
Lightbourne <61543506+Lightbourne@users.noreply.github.com>
2020-06-11 20:58:03 +0200

    Create pull_restart.sh

---
+++ pull_restart.sh
@@ -0,0 +1,5 @@
+sudo git stash push
+sudo git fetch --all
+sudo git reset --hard origin/deploy
+sudo systemctl restart packpng.service
+# requires the executing user to have nopasswd rights in sudoers for commands git and systemctl
