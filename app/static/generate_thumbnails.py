7bfd9b6f8b8565e8a061959bc239e46e16409388
Lightbourne <61543506+Lightbourne@users.noreply.github.com>
2020-06-11 21:07:11 +0200

    Create generate_thumbnails.py

---
+++ app/static/generate_thumbnails.py
@@ -0,0 +1,19 @@
+from PIL import Image, ImageOps
+from pathlib import Path
+
+sizes = [(128,128),]
+staticdir = Path(__file__).parent
+
+thumbnails = (staticdir / "thumbnails").glob('**/*')
+
+for thumb in thumbnails:
+    thumb.unlink()
+    
+files = (staticdir / "gallery").glob('**/*')#[str(p.as_posix()) for p in ]
+
+for image in files:
+    print(image)
+    for size in sizes:
+        im = Image.open(image)
+        im = ImageOps.fit(im, size, Image.ANTIALIAS)
+        im.save((staticdir / "thumbnails" / f"thumbnail_{image.name}").as_posix())
